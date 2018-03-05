287d7e3f37255c01a923707c567dbc7428af2dc2
ToBeAmped <martinja@oregonstate.edu>
2018-03-04 23:13:19 -0800

    Add server socket to server.py

--- src/client.py
+++ src/client.py
@@ -1,2 +1,11 @@
+#!/usr/bin/env python
 import socket
 import threading
+import sys
+
+if len(sys.argv) < 3:
+	print "usage: ./client.py port input_file"
+else: 
+	port = sys.argv[1]
+	inputFile = sys.argv[2]
+	# print inputFile	
--- src/server.py
+++ src/server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import socket
 import threading
 import sys
@@ -6,5 +7,18 @@ accounts = {}
 
 if len(sys.argv) < 2:
 	print "usage: ./server.py port"
+else: 
+	port = sys.argv[1]
+	socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	socket.bind(('localhost', int(port)))
+	socket.listen(5)
 	
-	
+	while True:
+		# accept connections from outside
+		(clientsocket, address) = socket.accept()
+		# now do something with the clientsocket
+		# in this case, we'll pretend this is a threaded server
+		print clientsocket 
+		print address
+		# ct = client_thread(clientsocket)
+		# ct.run()
