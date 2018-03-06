055ae4ac31072e4ac350fd17ec460f95efa93188
ToBeAmped <martinja@oregonstate.edu>
2018-03-06 13:30:10 -0800

    Handle client connectivity and threading for handling multiple clients at a time

--- src/client.py
+++ src/client.py
@@ -8,4 +8,25 @@ if len(sys.argv) < 3:
 else: 
 	port = sys.argv[1]
 	inputFile = sys.argv[2]
-	# print inputFile	
+	
+	try:
+		with open(inputFile) as file:
+			content = file.readlines()
+			content = [x.strip() for x in content]
+			
+		socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.connect(('127.0.0.1', int(port)))
+		
+		#Send data to server
+		for line in content:
+			line = line.split(" ")
+			socket.send(line)
+		
+		#Receive terminating messaage from server
+		st = socket.recv(100)
+		print st
+		socket.close()
+		exit(0)
+	except Exception as e:
+		print e
+		exit(1)
--- src/server.py
+++ src/server.py
@@ -1,24 +1,47 @@
 #!/usr/bin/env python
 import socket
 import threading
+import signal
 import sys
 
+def signal_handler(signal, frame):
+	print "\nJoining threads"
+	for thread in threads:
+		thread.join()
+	#write sorted data to log.txt
+	exit(0)
+	
+
+def handle_socket(socket):
+	st = socket.recv(1024)
+	print st
+	socket.send('OK\n')
+	socket.close()
+	
 accounts = {}
+threads = []
+
+#Initialize listener for signal interrupt
+signal.signal(signal.SIGINT, signal_handler)
 
 if len(sys.argv) < 2:
 	print "usage: ./server.py port"
 else: 
-	port = sys.argv[1]
-	socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	socket.bind(('localhost', int(port)))
-	socket.listen(5)
+	try:
+		port = sys.argv[1]
+		socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.bind(('127.0.0.1', int(port)))
+		socket.listen(5)
 	
-	while True:
-		# accept connections from outside
-		(clientsocket, address) = socket.accept()
-		# now do something with the clientsocket
-		# in this case, we'll pretend this is a threaded server
-		print clientsocket 
-		print address
-		# ct = client_thread(clientsocket)
-		# ct.run()
+		while True:
+			#Accept client connection
+			(client_socket, address) = socket.accept()
+			t = threading.Thread(target = handle_socket, args=(client_socket,))
+			threads.append(t)
+			t.start()
+			
+			
+	except  Exception as e:
+		print e
+		exit(1)
+		
